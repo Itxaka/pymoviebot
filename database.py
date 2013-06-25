e2ffb0535aa46f7d5afacccab815f273b02857a1
Itxaka <itxakaserrano@gmail.com>
2013-06-25 10:20:27 +0200

    fixed proxy settings, fixed vimeo reddit private

--- database.py
+++ database.py
@@ -23,11 +23,20 @@ This code works in conjuntion with pymoviebot.py
 
 Version = 1.1
 
+# get the proxy data and save it if necessary
+import urllib
+if urllib.getproxies()['http'] == "":
+    print "No proxies"
+else:
+    with open("praw.ini", "w") as f:
+        f.write("[reddit]\n")
+        f.write("http_proxy = " + urllib.getproxies()['http'])
+    
 import praw
 import sqlite3
 import logging
 import config
-import urllib
+
 
 # logging parameters below
 logging.basicConfig(filename=config.path + 'database.log',level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
@@ -36,10 +45,9 @@ logging.info("Program Start")
 user_agent = ("pymoviebot 1.1 by /u/itxaka")  # API guidelines enforce this
 
 r = praw.Reddit(user_agent=user_agent)
-praw.Config.http_proxy = urllib.getproxies()
 
 try:
-    r.login(config.user,config.password)  # login so we can enter vimeo subreddit
+    r.login(config.user, config.password)  # login so we can enter vimeo subreddit
 except (praw.errors.InvalidUser, praw.errors.InvalidUserPass):
     logging.error("User or password is incorrect, exiting...")
     exit(0)
@@ -57,9 +65,10 @@ def updateDb(reddit):
     subreddit = r.get_subreddit(reddit)
     for submission in subreddit.get_new(limit=1000):
         c.execute('INSERT INTO movie VALUES(?,?,?,?,?,?)', (submission.id,submission.title, submission.url, str(submission.author), submission.permalink, reddit))
+        print "record inserted"
         db.commit()
 
-updateDb("fullmoviesonvimeo")
+#updateDb("fullmoviesonvimeo") # commented as the subreddit is private
 updateDb("fullmoviesonyoutube")
 
 c.close()
