923da9431000a7123b704f101022758e24f9a78b
Itxaka <itxakaserrano@gmail.com>
2013-06-25 09:38:23 +0200

    New way of getting the database update
    Now we drop the database fully and get the whole database up to date.
    Also, removed the id checkinf to search for duplicated entries as we are
    starting new each time. It works with a proxy now. Removed extra fields
    in the database. Minimal login. Now requires user and password as
    fullmoviesonvimeo subreddit is private.

--- database.py
+++ database.py
@@ -21,59 +21,47 @@ This code will crawl around /r/fullmoviesonvimeo and /r/fullmoviesonyoutube and 
 This code works in conjuntion with pymoviebot.py
 """
 
-Version = 0.2
-
-# TODO: Try/catch if reddit is down or there is no connection
-# DONE! logging so we don't print anything to the screen
-# TODO: Catch database UNIQUE violation in case an id is duplicated
-# TODO: Fix the count at the end. Even if it doesn't insert anything, the numbers don't match. Dont know why.
+Version = 1.1
 
 import praw
 import sqlite3
 import logging
 import config
-
+import urllib
 
 # logging parameters below
 logging.basicConfig(filename=config.path + 'database.log',level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 logging.info("Program Start")
 
-user_agent = ("pymoviebot 0.1 by /u/itxaka")  # API guidelines enforce this
-count = 0
+user_agent = ("pymoviebot 1.1 by /u/itxaka")  # API guidelines enforce this
 
 r = praw.Reddit(user_agent=user_agent)
+praw.Config.http_proxy = urllib.getproxies()
+
+try:
+    r.login(config.user,config.password)  # login so we can enter vimeo subreddit
+except (praw.errors.InvalidUser, praw.errors.InvalidUserPass):
+    logging.error("User or password is incorrect, exiting...")
+    exit(0)
 
 db = sqlite3.connect("data.sql")
 c = db.cursor()
-c.execute("Create table if not exists movie(id text UNIQUE, name text, url text, author text, reddit_url text, subreddit text, status text)")
+try:
+    c.execute("DROP TABLE movie")
+except sqlite3.OperationalError:
+    pass
 
+c.execute("Create table if not exists movie(id text UNIQUE, name text, url text, author text, reddit_url text, subreddit text)")
 
 def updateDb(reddit):
-    global count
     subreddit = r.get_subreddit(reddit)
     for submission in subreddit.get_new(limit=1000):
-        c.execute('SELECT * FROM movie where id=?', (submission.id,))
-        if c.fetchone():  # check if the id is already in the database
-            count += 1  # if it was, add one so we can check the db at the end
-        else:
-            # use str() for the author as it won't work any other way
-            c.execute('INSERT INTO movie VALUES(?,?,?,?,?,?,?)', (submission.id,submission.title, submission.url, str(submission.author), submission.permalink, "fullmoviesonvimeo", "ok"))
-            db.commit()
+        c.execute('INSERT INTO movie VALUES(?,?,?,?,?,?)', (submission.id,submission.title, submission.url, str(submission.author), submission.permalink, reddit))
+        db.commit()
 
 updateDb("fullmoviesonvimeo")
 updateDb("fullmoviesonyoutube")
 
-# we will execute a little check in order to see if we found new items
-c.execute("SELECT COUNT(*) FROM movie")
-controlCheck = c.fetchone()[0]
-
-if controlCheck == count:
-    logging.info("Database is up to date")
-else:
-    logging.info("Database was updated")
-    logging.info("There was " + str(count) + " items before. There is " + str(controlCheck) + " items now.")
-
-# Don't forget to close the database (¬_¬)
 c.close()
 db.close()
 logging.info("Program End")
